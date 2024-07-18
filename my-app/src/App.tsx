a5a215da41b842a4cbda56a5af7096a2fe9ba635
Raffelke <2206801leunissen@zuyd.nl>
2024-07-18 21:45:08 +0200

    Hello world gemaakt voor ReactTS

---
+++ my-app/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css'; // Importeer de CSS
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World!</h1> {/* De Hello World boodschap */}
+        <p>Welcome to my React application.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
