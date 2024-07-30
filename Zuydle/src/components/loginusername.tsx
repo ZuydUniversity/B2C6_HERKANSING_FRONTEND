dee42641ab290668374d7cf49c3d31c3ca5fff2c
XergSolana <124872583+XergSolana@users.noreply.github.com>
2024-07-30 18:18:51 +0200

    Login Username Component
    Component for a textbox where the user can put in their username

---
+++ Zuydle/src/components/loginusername.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserLoginComponent: React.FC<{}> = () => {
+  return (
+    <div>
+     <input type = "text" id = "username" name = "username" />
+    </div>
+  );
+};
+
+export default UserLoginComponent;
