61794a78c153d5e82ea950fcdb20bbe70ebbfd11
Raffelke <2206801leunissen@zuyd.nl>
2024-07-31 10:28:02 +0200

    Laatste veranderingen

--- Zuydle/src/Title.tsx
+++ Zuydle/src/Title.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import styles from './Title.module.css';
+import React from "react";
+import styles from "./Title.module.css";
 
 interface TitleProperties {
   text: string;
-  size?: 'small' | 'medium' | 'large';
+  size?: "small" | "medium" | "large";
 }
 
-const Title: React.FC<TitleProperties> = ({ text, size = 'medium' }) => {
+const Title: React.FC<TitleProperties> = ({ text, size = "medium" }) => {
   let fontSize: string;
   let className = styles.title;
 
   switch (size) {
-    case 'small':
-      fontSize = '1.5rem';
+    case "small":
+      fontSize = "1.5rem";
       break;
-    case 'medium':
-      fontSize = '2rem';
+    case "medium":
+      fontSize = "2rem";
       break;
-    case 'large':
-      fontSize = '4rem';
+    case "large":
+      fontSize = "4rem";
       className = `${styles.title} ${styles.largeTitle}`;
       break;
     default:
-      fontSize = '2rem';
+      fontSize = "2rem";
   }
 
   return (
