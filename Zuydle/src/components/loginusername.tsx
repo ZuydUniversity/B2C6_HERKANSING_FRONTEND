889fd2a8179126b792048e71f0a073bf9d322089
XergSolana <124872583+XergSolana@users.noreply.github.com>
2024-08-11 22:55:00 +0200

    Hook integration
    integrated a hook within the username component

--- Zuydle/src/components/loginusername.tsx
+++ Zuydle/src/components/loginusername.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 const UserLoginComponent: React.FC<{}> = () => {
+  const[username, setUsername] = useState('')
+
+  // React input handler that updates the hook
+  const handleInputChange = 
+  (e: React.ChangeEvent<HTMLInputElement>) => {
+      setUsername(e.target.value);
+  };
+
   return (
     <div>
-     <input type = "text" id = "username" name = "username" />
+     <input type = "text" value={username} 
+          onChange ={handleInputChange} 
+          placeholder="Username"/>
+        <p> {username}</p>
     </div>
   );
 };
