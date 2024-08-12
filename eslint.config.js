2fb43bc003e2893ba18619fe09a8654c05095c5c
2205481Dakka <2205481vonk@zuyd.nl>
2024-08-12 02:10:22 +0200

    linter aanpassingen

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+const react = require('eslint-plugin-react');
+const ts = require('@typescript-eslint/eslint-plugin');
+const globals = require('globals');
+
+module.exports = [
+  {
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'], // Target files
+    plugins: {
+      react, // Register the react plugin
+      '@typescript-eslint': ts, // Register the TypeScript plugin
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true, // Enable JSX
+        },
+      },
+      globals: {
+        ...globals.browser, // Include browser globals like window, document, etc.
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off', // Disable the rule since React 17+ does not require React in scope
+      'react/jsx-uses-react': 'off', // Disable rule for React 17+ JSX Transform
+      'react/jsx-uses-vars': 'error', // Prevent variables used in JSX from being marked as unused
+      '@typescript-eslint/no-unused-vars': 'error', // Prevent unused variables in TypeScript
+    },
+  },
+];
