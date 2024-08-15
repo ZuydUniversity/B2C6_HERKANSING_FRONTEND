a5313b994f3c95d0afe78a567e8362ed0f3cd8a5
2205481Dakka <2205481vonk@zuyd.nl>
2024-08-16 00:53:04 +0200

    exclude node modules

--- Zuydle/src/App.tsx
+++ Zuydle/src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css'
-import PasswordInput from './Password'
 
 function App() {
     return (
     <>     
       <div className="app">
-            <PasswordInput label="Password:" placeholder="Enter your password" />
+          
         </div>
     </>
   )
